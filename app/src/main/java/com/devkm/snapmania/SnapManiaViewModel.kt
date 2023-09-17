f077810ad8a7e4e5bb8d36aff7041c6cbc069862
mahavir <mahavirkumarbabu@gmail.com>
2023-09-17 17:24:12 +0530

    retrive and display user posts

--- app/src/main/java/com/devkm/snapmania/SnapManiaViewModel.kt
+++ app/src/main/java/com/devkm/snapmania/SnapManiaViewModel.kt
@@ -1,6 +1,7 @@
 package com.devkm.snapmania
 
 import android.net.Uri
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.devkm.snapmania.data.Event
@@ -8,6 +9,7 @@ import com.devkm.snapmania.data.PostData
 import com.devkm.snapmania.data.UserData
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.QuerySnapshot
 import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.storage.FirebaseStorage
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -29,6 +31,10 @@ class SnapManiaViewModel @Inject constructor(
     val inProgress = mutableStateOf(false)
     val userData = mutableStateOf<UserData?>(null)
     val popupNotification = mutableStateOf<Event<String>?>(null)
+
+
+    val refreshPostsProgress = mutableStateOf(false)
+    val posts = mutableStateOf<List<PostData>>(listOf())
 
     init {
 //        firebaseAuth.signOut()
@@ -145,7 +151,7 @@ class SnapManiaViewModel @Inject constructor(
                 val user = it.toObject<UserData>()
                 userData.value = user
                 inProgress.value = false
-//                refreshPosts()
+                refreshPosts()
 //                getPersonalizedFeed()
 //                getFollowers(user?.userId)
             }
@@ -235,7 +241,7 @@ class SnapManiaViewModel @Inject constructor(
                 .addOnSuccessListener {
                     popupNotification.value = Event("Post successfully created")
                     inProgress.value = false
-//                    refreshPosts()
+                    refreshPosts()
                     onPostSuccess.invoke()
                 }
                 .addOnFailureListener { exc ->
@@ -250,4 +256,33 @@ class SnapManiaViewModel @Inject constructor(
         }
     }
 
+
+    private fun refreshPosts() {
+        val currentUid = firebaseAuth.currentUser?.uid
+        if (currentUid != null) {
+            refreshPostsProgress.value = true
+            firebaseFirestoreDb.collection(POSTS).whereEqualTo("userId", currentUid).get()
+                .addOnSuccessListener { documents ->
+                    convertPosts(documents, posts)
+                    refreshPostsProgress.value = false
+                }
+                .addOnFailureListener { exc ->
+                    handleException(exc, "Cannot fetch posts")
+                    refreshPostsProgress.value = false
+                }
+        } else {
+            handleException(customMessage = "Error: username unavailable. Unable to refresh posts")
+            onLogout()
+        }
+    }
+
+    private fun convertPosts(documents: QuerySnapshot, outState: MutableState<List<PostData>>) {
+        val newPosts = mutableListOf<PostData>()
+        documents.forEach { doc ->
+            val post = doc.toObject<PostData>()
+            newPosts.add(post)
+        }
+        val sortedPosts = newPosts.sortedByDescending { it.time }
+        outState.value = sortedPosts
+    }
 }
